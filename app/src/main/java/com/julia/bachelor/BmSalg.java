41dd559ed3b2ccfae628c240f56b6f29d1be2fc3
Meyben <ida@honningbier.no>
2019-01-22 17:22:38 +0100

    legger til activity HjemmeSalg

--- app/src/main/java/com/julia/bachelor/BmSalg.java
+++ app/src/main/java/com/julia/bachelor/BmSalg.java
@@ -2,6 +2,7 @@ package com.julia.bachelor;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class BmSalg extends Activity {
 
@@ -9,5 +10,18 @@ public class BmSalg extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bm_salg);
+
+        getActionBar().setDisplayShowHomeEnabled(true);
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+        if(id == android.R.id.home){
+            this.finish();
+        }
+        return onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/julia/bachelor/HjemmeSalg.java
+++ app/src/main/java/com/julia/bachelor/HjemmeSalg.java
@@ -2,6 +2,9 @@ package com.julia.bachelor;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class HjemmeSalg extends Activity {
 
@@ -9,5 +12,19 @@ public class HjemmeSalg extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hjemme_salg);
+
+    getActionBar().setDisplayShowHomeEnabled(true);
+    getActionBar().setDisplayHomeAsUpEnabled(true);
+
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+        if(id == android.R.id.home){
+            this.finish();
+        }
+        return onOptionsItemSelected(item);
+    }
+
 }

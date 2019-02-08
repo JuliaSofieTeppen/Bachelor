dc6ede038418662535716870ef3a2753a610ba1d
Meyben <ida@honningbier.no>
2019-02-08 11:11:47 +0100

    oppdaterer hjemmesalg aktiviteten

--- app/src/main/java/com/julia/bachelor/Innstillinger.java
+++ app/src/main/java/com/julia/bachelor/Innstillinger.java
@@ -2,9 +2,21 @@ package com.julia.bachelor;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
 import android.view.MenuItem;
+import android.view.View;
 
 public class Innstillinger extends Activity {
+    ConstraintLayout som1kg;
+    ConstraintLayout som05kg;
+    ConstraintLayout som025kg;
+    ConstraintLayout lyng1kg;
+    ConstraintLayout lyng05kg;
+    ConstraintLayout lyng025kg;
+    ConstraintLayout Ingf05kg;
+    ConstraintLayout Ingf025kg;
+    ConstraintLayout flyt;
+    ConstraintLayout endreverdier;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,6 +24,98 @@ public class Innstillinger extends Activity {
         setContentView(R.layout.activity_innstillinger);
 
         getActionBar().setDisplayHomeAsUpEnabled(true);
+
+        som1kg = findViewById(R.id.som1kg);
+        som05kg = findViewById(R.id.som05kg);
+        som025kg = findViewById(R.id.som025kg);
+        lyng1kg = findViewById(R.id.lyng1kg);
+        lyng05kg = findViewById(R.id.lyng05kg);
+        lyng025kg = findViewById(R.id.lyng025kg);
+        Ingf05kg = findViewById(R.id.ingf05kg);
+        Ingf025kg = findViewById(R.id.ingf025kg);
+        flyt = findViewById(R.id.flyt);
+        endreverdier = findViewById(R.id.endreverdier);
+
+        endreverdier.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ConstraintLayout exe = findViewById(R.id.bigextend);
+                extend(exe);
+            }
+        });
+        som1kg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ConstraintLayout exe = findViewById(R.id.extend);
+                extend(exe);
+            }
+        });
+
+        som05kg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ConstraintLayout exe = findViewById(R.id.extend2);
+                extend(exe);
+            }
+        });
+        som025kg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ConstraintLayout exe = findViewById(R.id.extend3);
+                extend(exe);
+            }
+        });
+
+        lyng1kg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ConstraintLayout exe = findViewById(R.id.extend4);
+                extend(exe);
+            }
+        });
+        lyng05kg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ConstraintLayout exe = findViewById(R.id.extend5);
+                extend(exe);
+            }
+        });
+        lyng025kg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ConstraintLayout exe = findViewById(R.id.extend6);
+                extend(exe);
+            }
+        });
+        Ingf05kg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ConstraintLayout exe =findViewById(R.id.extend7);
+                extend(exe);
+            }
+        });
+        Ingf025kg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ConstraintLayout exe = findViewById(R.id.extend8);
+                extend(exe);
+            }
+        });
+        flyt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ConstraintLayout exe = findViewById(R.id.extend9);
+                extend(exe);
+            }
+        });
+    }
+
+    public void extend(ConstraintLayout exe){
+        if(exe.isShown()){
+            exe.setVisibility(View.GONE);
+        }else {
+            exe.setVisibility(View.VISIBLE);
+        }
     }
 
     @Override

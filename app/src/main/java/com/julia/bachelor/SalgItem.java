0997293256c296dd565179ff996a018cfd04ecf1
JuliaSofieTeppen <julia.teppen@gmail.com>
2019-02-28 14:31:15 +0100

    i broke it

--- app/src/main/java/com/julia/bachelor/SalgItem.java
+++ app/src/main/java/com/julia/bachelor/SalgItem.java
@@ -1,9 +1,10 @@
 package com.julia.bachelor;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class SalgItem extends AppCompatActivity {
+public class SalgItem extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {

a8afe9c05dd63b79ccb67fa9d60edcf603135673
Meyben <ida@honningbier.no>
2019-03-05 11:32:24 +0100

    oppdaterer hjemmesalg aktiviteten

--- app/src/main/java/com/julia/bachelor/LoadContent.java
+++ app/src/main/java/com/julia/bachelor/LoadContent.java
@@ -23,6 +23,7 @@ public class LoadContent extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_load_content);
+        getActionBar().hide();
         Database database;
         database = new Database();
         database.getHonningType();

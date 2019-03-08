b1f7481ae2192d63fc07220a25b67d228b7a84c5
JuliaSofieTeppen <julia.teppen@gmail.com>
2019-03-08 10:29:05 +0100

    cleanups :))))

--- app/src/main/java/com/julia/bachelor/SkalBliAddBeholdning.java
+++ app/src/main/java/com/julia/bachelor/SkalBliAddBeholdning.java
@@ -1,15 +1,11 @@
 package com.julia.bachelor;
 
-import android.content.Context;
-import android.net.Uri;
-import android.os.Bundle;
 import android.app.Fragment;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -18,43 +14,19 @@ import java.util.List;
 
 
 public class SkalBliAddBeholdning extends Fragment {
-    private static final String ARG_SECTION_NUMBER = "section_number";
-    EditText dato;
-    EditText som1kg;
-    EditText som05kg;
-    EditText som025kg;
-    EditText lyng1kg;
-    EditText lyng05kg;
-    EditText lyng025kg;
-    EditText ingf05kg;
-    EditText ingf025kg;
-    EditText flytende;
+    EditText dato, som1kg, som05kg, som025kg, lyng1kg, lyng05kg, lyng025kg, ingf05kg, ingf025kg, flytende;
     List<EditText> verdier;
-    Database db;
 
-    public SkalBliAddBeholdning() {
-        // Required empty public constructor
+    public SkalBliAddBeholdning() {}
+
+    public static SkalBliAddBeholdning newInstance() {
+        return new SkalBliAddBeholdning();
     }
-
-
-    public static SkalBliAddBeholdning newInstance(int sectionNumber) {
-        SkalBliAddBeholdning fragment = new SkalBliAddBeholdning();
-        Bundle args = new Bundle();
-        args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
         View rootView =  inflater.inflate(R.layout.fragment_skal_bli_add_beholdning, container, false);
-
-
-        db = new Database();
-        db.getHonningType();
         dato = rootView.findViewById(R.id.Bdato);
         som1kg = rootView.findViewById(R.id.Bsom1kg);
         som05kg = rootView.findViewById(R.id.Bsom05kg);
@@ -68,6 +40,41 @@ public class SkalBliAddBeholdning extends Fragment {
         verdier = new ArrayList<>(Arrays.asList(som1kg, som05kg, som025kg, lyng1kg, lyng05kg, lyng025kg, ingf05kg, ingf025kg, flytende));
 
         return rootView;
+    }
+
+    public void lagre(View v) {
+        int tell = 0;
+        if (Beregninger.checkDate(dato.getText().toString())) {
+            for (EditText verdi : verdier) {
+                if (verdi.getText().toString().equals("")) {
+                    verdi.setText("0");
+                } else {
+                    tell++;
+                }
+            }
+            if (tell == 0) {
+                Toast.makeText(getActivity(),"Legg til minst et produkt", Toast.LENGTH_SHORT).show();
+            } else {
+                insertIntoDB();
+                Toast.makeText(getActivity() ,"Beholdning lagret", Toast.LENGTH_SHORT).show();
+                //this.finish();
+            }
+        } else {
+            Toast.makeText(getActivity(),"Ugyldig dato", Toast.LENGTH_SHORT).show();
+        }
+    }
+    String getBeholdning(){
+        String[] strings = {"Sommer","SommerH","SommerK","Lyng","LyngH","LyngK","IngeferH","IngeferK","Flytende"};
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < strings.length; i++){
+            sb.append(strings[i]).append("=").append(verdier.get(i).getText().toString()).append("&");
+        }
+        return sb.toString();
+    }
+
+    void insertIntoDB(){
+        Database.executeOnDB("http://www.honningbier.no/PHP/SalgIn.php/?Dato=" + dato.getText().toString());
+        Database.executeOnDB("http://www.honningbier.no/PHP/BeholdningIn.php/?" + getBeholdning() + "&Dato=" + dato.getText().toString());
     }
 
 }

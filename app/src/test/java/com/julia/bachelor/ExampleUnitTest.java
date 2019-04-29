fc28bb1992e368743a32d6cf9f810e92f975a96c
JuliaSofieTeppen <julia.teppen@gmail.com>
2019-04-29 12:13:52 +0200

    Something is wrong :/

--- app/src/test/java/com/julia/bachelor/ExampleUnitTest.java
+++ app/src/test/java/com/julia/bachelor/ExampleUnitTest.java
@@ -3,14 +3,15 @@ package com.julia.bachelor;
 import com.julia.bachelor.helperClass.Annet;
 import com.julia.bachelor.helperClass.BondensMarked;
 import com.julia.bachelor.helperClass.Hjemme;
-import com.julia.bachelor.helperClass.Salg;
 import com.julia.bachelor.helperClass.SalgTemplate;
+import com.julia.bachelor.helperClass.SortedObjects;
 import com.julia.bachelor.helperClass.Videresalg;
 
 import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import static org.junit.Assert.*;
 
@@ -20,23 +21,58 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
-    private ArrayList<SalgTemplate> salg;
+    private ArrayList<SalgTemplate> sale;
 
     @Before
-    private void setVariables(){
-        String varer = "1-1,2-3,3-0,4-5,5-0,6-0,7-0,8-0,9-0,";
+    public void setVariables(){
+        String products = "1-1,2-3,3-0,4-5,5-0,6-0,7-0,8-0,9-0,";
         String date = Beregninger.getDate();
-        String Betaling = "Kort";
-        String kunde = "Sam Knight";
-        salg = new ArrayList<>();
-        SalgTemplate hjemme = new Hjemme();
+        String payment = "Kort";
+        String customer = "Sam Knight";
+        sale = new ArrayList<>();
+        SalgTemplate home = new Hjemme();
         SalgTemplate bm = new BondensMarked();
-        SalgTemplate videre = new Videresalg();
-        SalgTemplate annet = new Annet();
+        SalgTemplate resell = new Videresalg();
+        SalgTemplate other = new Annet();
+        sale.addAll(Arrays.asList(home, bm, resell, other));
+        for(SalgTemplate sale : sale){
+            sale.setBelop(100);
+            sale.setBetaling(payment);
+            sale.setDato(date);
+            sale.setKunde(customer);
+            if (!(sale instanceof Annet))
+            sale.setVarer(products);
+        }
+        other.setVarer("Bifolk-12,Voks-0,Pollinering-0,Dronninger-0,");
     }
 
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    public void add_isCorrect() {
+        SortedObjects sorted = new SortedObjects(true);
+        for(SalgTemplate sale : sale){
+            sorted.add(sale);
+        }
+        assertEquals(400, sorted.getBelop());
+        assertEquals(400 , sorted.getBetalings()[1]);
+        assertEquals(0, sorted.getBetalings()[0]);
+        int[] amounts = new int[9];
+        int[] expected = {3,3*3,0,5*3,0,0,0,0,0};
+        long start = System.nanoTime();
+        String products = sorted.getVarer();
+        String[] productAmount = products.split(",");
+        for(int i = 0; i < productAmount.length; i++){
+            String[] amount = productAmount[i].split("-");
+            amounts[i]=Integer.parseInt(amount[1]);
+        }
+        long end = System.nanoTime();
+        assertEquals(expected, amounts);
+        long elapsed = end-start;
+        System.out.println("Time elapsed: " + elapsed);
+    }
+
+    @Test(timeout=1000)
+    public void timeTest(){
+        MainActivity main = new MainActivity();
+        main.fetch();
     }
 }

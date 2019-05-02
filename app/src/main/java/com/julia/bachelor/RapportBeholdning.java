6d64e14011e3dbfc9cc39a62ebdd72127c974178
Meyben <ida@honningbier.no>
2019-05-02 19:13:11 +0200

    oppdatere beholdning

---
+++ app/src/main/java/com/julia/bachelor/RapportBeholdning.java
@@ -0,0 +1,13 @@
+package com.julia.bachelor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RapportBeholdning extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rapport_beholdning);
+    }
+}
